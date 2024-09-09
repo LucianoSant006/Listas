6562b77946ca49f579f89973a4a8a219cc714b59
LucianoSant006 <santiagoluciano383@gmail.com>
2024-09-08 22:37:28 -0300

    Fineshed Exerc 01_list Employee

--- src/Program.java
+++ src/Program.java
@@ -7,52 +7,64 @@ import entities.Employee;
 
 public class Program {
 
-	public static void main(String[] Args) { 
-	Locale.setDefault(Locale.US);
-	Scanner sc = new Scanner(System.in);
-	
-	
-	
-	List<Employee> list = new ArrayList();
-	 
-	System.out.printf("How manu empployees will be registered");
-	int N = sc.nextInt();
-	
-	for(int i = 0;i < N;i++) {
-		
-		
-	System.out.printf("Employee #%d",i);
-	System.out.println();
-	System.out.print("Id: ");
-	int id = sc.nextInt();
-    sc.nextLine();
-	System.out.printf("Name: ");
-	String name= sc.next();
-	System.out.printf("Salary: ");
-	Double salary= sc.nextDouble();
-	
-	Employee emplo = new Employee(id, name, salary);
-	list.add(emplo);
-		
-	}
-	
-	System.out.println("Enter the employee id that will have salary increase: ");
-	int idd = sc.nextInt();
-	Integer pos = positionId(list,idd);
-	
+	public static void main(String[] Args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
 
-}
-public Integer positionId(List<Employee> list,int id) {
-		
-		for(int i =0; i < list.size();i++) {
+		List<Employee> list = new ArrayList();
+
+		System.out.printf("How many employees will be registered? ");
+		int N = sc.nextInt();
+
+		for (int i = 0; i < N; i++) {
 			
-			if(list.get(i).getId() == id) {
-				return i;
-			}
-			return null;
+			System.out.println();
+			System.out.printf("Employee #%d", i + 1);
+			System.out.println();
+			System.out.print("Id: ");
+			int id = sc.nextInt();
+			System.out.printf("Name: ");
+			String name = sc.next();
+			sc.nextLine();
+			System.out.printf("Salary: ");
+			Double salary = sc.nextDouble();
+
+			Employee emplo = new Employee(id, name, salary);
+			list.add(emplo);
 			
 		}
-	
-		
+		System.out.println();
+		System.out.printf("Enter the employee id that will have salary increase: ");
+		int idd = sc.nextInt();
+		Integer pos = positionId(list, idd);
+		if (pos == null) {
+			System.out.println("This id does not exist!");
+		} else {
+
+			System.out.print("Enter the percntage: ");
+			double percent = sc.nextDouble();
+			list.get(pos).Percentual(percent);
+		}
+		System.out.println();
+		System.out.println("List of employees:");
+		for (Employee emp : list) {
+			System.out.println(emp);
+		}
+		sc.close();
+
 	}
-	
+
+	public static Integer positionId(List<Employee> list, int id) {
+
+		for (int i = 0; i < list.size(); i++) {
+
+			if (list.get(i).getId() == id) {
+				return i;
+			}
+
+		}
+		return null;
+
+	}
+
+}
--- src/entities/Employee.java
+++ src/entities/Employee.java
@@ -5,38 +5,45 @@ public class Employee {
 	private Integer id;
 	private String name;
 	private Double salary;
-	
+
 	public Employee() {
-		
+
 	}
-	
+
 	public Employee(Integer id, String name, Double salary) {
 		this.id = id;
 		this.name = name;
 		this.salary = salary;
 	}
+
 	public Integer getId() {
 		return id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public Double getSalary() {
 		return salary;
 	}
+
 	public void setSalary(Double salary) {
 		this.salary = salary;
 	}
-	
-	
-	
-	public void Percentual (double  percentage) {
-		
-		percentage  =salary * 10 / 100;
-		
+
+	public void Percentual(double percentage) {
+
+		salary += salary * percentage / 100.0;
+
 	}
-	
+
+	public String toString() {
+		return id + ", " + name + ", " + String.format("%.2f", salary);
+	}
+
 }
